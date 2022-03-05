8bdabed33a9b69eea45db275b08cb94f3e2e8992
seungha <frazzle124@gmail.com>
2022-03-05 19:39:20 +0900

    데이터 바인딩

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar,Container,Nav,NavDropdown,Form,Button,FormControl } from 'react-bootstrap';
+import { useState } from 'react';
+import Data from './data.js';
 
 function App() {
+
+  let [shoes, shoes변경] = useState(Data);
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">my shop</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+
+<div className='mainbox'>
+    <h2 style = {{padding : 30}}> Hello </h2>
+    <p> 안녕하세요 </p>
+    <Button variant="primary">Primary</Button>{' '}
+</div>
+
+<div className="container">
+
+        <div className="row">
+         {
+           shoes.map((a, i)=>{
+             return (
+               <Component shoes = {shoes[i]}></Component>
+             )
+            
+           })
+         }
+        </div>
+      </div>
+ </div>
   );
+}
+
+function Component(props){
+  return (
+    <div className="col-md-4">
+            <img src={props.shoes.img} width="100%" />
+            <h4>{props.shoes.title}</h4>
+            <p>{props.shoes.content}</p>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+//var name = 'kim'; 
+//var name1 = 'park;
+// export default name; // 다른 파일에서 name을 가져다 사용할 수 있다. 
+// export {name, name1} // 내보낼것이 많다면 오브젝트러첨 중괄호 안에 사용
+export default[
+
+    // 파일쪼갤때 사용
+    // export 파일을 내뱉을때
+    {
+      id : 0,
+      title : "White and Black",
+      content : "Born in France",
+      price : 120000,
+      img : "https://codingapple1.github.io/shop/shoes1.jpg"
+    },
+  
+    {
+      id : 1,
+      title : "Red Knit",
+      content : "Born in Seoul",
+      price : 110000,
+      img :  "https://codingapple1.github.io/shop/shoes2.jpg"
+    },
+  
+    {
+      id : 2,
+      title : "Grey Yordan",
+      content : "Born in the States",
+      price : 130000,
+      img : "https://codingapple1.github.io/shop/shoes3.jpg"
+    }
+  ] 
